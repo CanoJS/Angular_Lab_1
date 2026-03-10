9735dc5d448a73642577ebcdf56511f0a2643374
Canoghst <134983011+CanoJS@users.noreply.github.com>
2026-03-10 10:25:26 -0600

    Cambios en el frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,6 +16,9 @@ export class App {
   meals:any[] = [];
   selectedMeal:any = null;
 
+  currentPage = 1;
+  pageSize = 3;
+
   constructor(private mealService: MealService){}
 
   async search(event:any){
@@ -25,6 +28,34 @@ export class App {
     if(!query) return;
 
     this.meals = await this.mealService.searchMeals(query);
+
+    this.currentPage = 1;
+
+  }
+
+  get paginatedMeals(){
+
+    const start = (this.currentPage - 1) * this.pageSize;
+
+    return this.meals.slice(start, start + this.pageSize);
+
+  }
+
+  next(){
+
+    const maxPage = Math.ceil(this.meals.length / this.pageSize);
+
+    if(this.currentPage < maxPage){
+      this.currentPage++;
+    }
+
+  }
+
+  prev(){
+
+    if(this.currentPage > 1){
+      this.currentPage--;
+    }
 
   }
 
