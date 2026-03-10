2cfc0750a36696c8faf98eadd1748ac81d4dacc0
Canoghst <134983011+CanoJS@users.noreply.github.com>
2026-03-10 09:58:39 -0600

    Cambios en services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Modal } from './components/modal/modal';
+import { MealService } from './services/api';
 
 @Component({
   selector: 'app-root',
@@ -15,32 +16,24 @@ export class App {
   meals:any[] = [];
   selectedMeal:any = null;
 
-  search(event:any){
+  constructor(private mealService: MealService){}
+
+  async search(event:any){
 
     const query = event.target.value;
 
     if(!query) return;
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
-    .then(res => res.json())
-    .then(data => {
-
-      this.meals = data.meals || [];
-
-    });
+    this.meals = await this.mealService.searchMeals(query);
 
   }
 
   openModal(meal:any){
-
     this.selectedMeal = meal;
-
   }
 
   closeModal(){
-
     this.selectedMeal = null;
-
   }
 
 }
--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 
-export class ApiService {
+export class MealService {
 
-  url = "https://rickandmortyapi.com/api/character";
+  async searchMeals(query:string){
 
-  constructor(private http: HttpClient) {}
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+    );
 
-  getCharacters(page:number=1){
-    return this.http.get<any>(`${this.url}?page=${page}`);
-  }
+    const data = await response.json();
 
-  searchCharacters(name:string){
-    return this.http.get<any>(`${this.url}?name=${name}`);
+    return data.meals || [];
+
   }
 
 }
